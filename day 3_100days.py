466003d606a75de99157c49b4fe89d1bcff1cc0a
Yo0GuitarIT <118150842+Yo0GuitarIT@users.noreply.github.com>
2022-11-15 00:05:49 +0800

    Create day 3_100days.py

---
+++ day 3_100days.py
@@ -0,0 +1,8 @@
+Food = input("Name a food > ")
+Plant = input("Name a type of plant > ")
+Cooking = input("Name a  method of cooking >")
+Des = input("What word describe burned food? > ")
+Item = input("Name a DIY item > ")
+
+print("\nMENU")
+print(Cooking,Food,"with",Des,Plant,"on a bed of",Item)
