f7dcf8614d0a41c8a672320ae1c0cbb13f3afb1d
Yo0GuitarIT <118150842+Yo0GuitarIT@users.noreply.github.com>
2022-11-15 00:09:15 +0800

    Create day 5_100days.py

---
+++ day 5_100days.py
@@ -0,0 +1,24 @@
+print("Marvel Movie Character Creator")
+
+A1 = input("Do you like 'hanging around'?:") 
+if A1 == "Yes":
+	print("You are a a spiderman")
+else:
+  print("Then you're not Spider-man")
+print()
+
+
+A2 = input("Do you have a 'gravelly' voice?:")
+if A2 == "Yes":
+	print("You are Korg")
+else:
+	print("Aww, then you're not Korg")
+print()
+
+
+
+A3 = input("Do you often feel 'Marvelous'?:")
+if A3 =="Yes":
+	print("Aha! You're Captain Marvel! Hi!")
+else:
+	print("Bye!!!")
