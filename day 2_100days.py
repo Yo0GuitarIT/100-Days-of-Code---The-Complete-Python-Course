4515035f6b88bf627e986b8a2bab478b0a51193a
Yo0GuitarIT <118150842+Yo0GuitarIT@users.noreply.github.com>
2022-11-15 00:04:31 +0800

    Create day 2_100days.py

---
+++ day 2_100days.py
@@ -0,0 +1,15 @@
+print("Getting to know you!\n")
+
+Name = input("What's your name? ")
+Food = input("What's your favorite food? ")
+Music = input("What's yput favorite music? ")
+Place = input("Where do you live? ")
+
+print("\nYou are")
+print(Name)
+print("You're probably hungry for")
+print(Food)
+print("and you're definitely getting your groove on to")
+print(Music)
+print("living in the amazing")
+print(Place)
