71ff8d6cb33882c0392950a1781ab3f25bd521d1
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2022-11-27 22:49:02 +0800

    Create day 031_100days.py

---
+++ day 031_100days.py
@@ -0,0 +1,33 @@
+tittle = "🚀🚀🚀Music app🚀🚀🚀"
+artist = "🔥▶	Radio Gaga"
+song = "Queen"
+a = "PREV"
+b = "NEXT"
+c = "PAUSE"
+print(f"{tittle:^30}")
+print(artist)
+print("\033[1;33m",f"{song:>8}\n")
+print()
+print("\033[0m",f"{a:<30}")
+print("\033[1;32m",f"{b:^30}")
+print("\033[1;31m",f"{c:>30}\n")
+print("\033[0m","###################################\n")
+
+tittle2="WELCOME TO"
+book = "--   ARMBOOK   --"
+s1 = "Definitely not a rip off of"
+s2 = "a certain other social"
+s3 = "network site."
+h = "honest."
+username = "Username:"
+password = "Password:"
+
+print(f"{tittle2:^40}")
+print("\033[1;34m",f"{book:^40}")
+print("\033[1;33m",f"{s1:>40}")
+print(f"{s2:>40}")
+print(f"{s3:>40}\n")
+print("\033[1;31m",f"{h:^40}")
+print("\033[0m")
+print(f"{username:^40}")
+print(f"{password:^40}")
