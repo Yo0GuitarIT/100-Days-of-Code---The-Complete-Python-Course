9731bd30cf9a1b09341f3c20fa8afa90d2dc9533
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-02-02 14:36:03 +0800

    Update and rename day 067_100days(TBC).py to day 067_100days.py

--- day 067_100days.py
+++ day 067_100days.py
@@ -25,41 +25,35 @@ tk.mainloop()
 
 window = tk.Tk()
 window.title("Guess Who?")
-window.geometry("300x300")
+window.geometry("400x200")
+label = "Guess Who?"
 
-firstlabel = tk.Label(text = "Guess Who?")
+def imageShow():
+	person = text.get("1.0","end")
+	if person.lower().strip() == "mo":
+		canvas.itemconfig(container, image = mo)
+	elif person.lower().strip() == "katie":
+		canvas.itemconfig(container, image = katie)
+	elif person.lower().strip() == "gerald":
+		canvas.itemconfig(container, image = gerald)
+	elif person.lower().strip() == "charlotte":
+		canvas.itemconfig(container, image = charlotte)
+	else:
+		firstlabel["text"] = "Unable to find user."
+
+firstlabel = tk.Label(text = label)
 firstlabel.pack()
-
-a = tk.StringVar()
-a.set('')
-
-def show():
-	name = a.get()
-	print(name)
-	a.set('')
-	
-def changeImage():
-	canvas.itemconfig(container, image = kate)
-
-wordentry = tk.Entry(window, textvariable = a)
-wordentry.pack()
-
-btn = tk.Button(text="Find",command = changeImage)
-btn.pack()
-
-canvas = tk.Canvas(window,width=300,heigh = 300)
+text = tk.Text(window,height=1,width=30)
+text.pack()
+button = tk.Button(text="Find",command = imageShow)
+button.pack()
+canvas = tk.Canvas(window,height=400,width=180)
 canvas.pack()
-
 charlotte = tk.PhotoImage(file = "charlotte.png")
-charlotte = charlotte.subsample(4)
 gerald = tk.PhotoImage(file = "gerald.png")
-gerald = gerald.subsample(4)
+katie = tk.PhotoImage(file = "katie.png")
 mo = tk.PhotoImage(file = "mo.png")
-mo = mo.subsample(4)
-kate = tk.PhotoImage(file = "katie.png")
-kate = kate.subsample(4)
 
-container = canvas.create_image(150,0, image = mo)
-
+container = canvas.create_image(200,0,image = mo)
 
 window.mainloop()
